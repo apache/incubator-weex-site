6578cd5c29ade4a5a74d61e647eb3ae71dedb1fd
york.sy <york.sy@alibaba-inc.com>
2019-07-26 16:32:32 +0800

    Remove release procedure and roadmap as they are moved to other places.
    * https://cwiki.apache.org/confluence/x/jJBTBw
    * https://cwiki.apache.org/confluence/x/_I5TBw

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -178,8 +178,6 @@ module.exports = {
             ['extension', 'VSCode Extension']
           ],
           '/community/': [
-            ['release-procedure', 'Release Procedure'],
-            ['roadmap', 'Roadmap'], 
             ['weex-third-party-extensions', 'Plugins Market']],
           '/blog/': [
             ['write-a-blog', 'Write a Blog'],
@@ -350,8 +348,6 @@ module.exports = {
             ['extension', 'VSCode 插件']
           ],
           '/zh/community/': [
-            ['release-procedure', 'Apache Release 流程'],
-            ['roadmap', '路线图'], 
             ['weex-third-party-extensions', '插件市场']],
           '/zh/blog/': [
             ['write-a-blog', '写一篇博客'],
